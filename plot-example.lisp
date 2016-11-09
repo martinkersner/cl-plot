d06c9ab3f20e262b05ff33b81bfb5d4440c86fa4
Martin Keršner <m.kersner@gmail.com>
2016-11-09 23:47:39 +0900

    example script

---
+++ plot-example.lisp
@@ -0,0 +1,19 @@
+;;;; Martin Kersner, m.kersner@gmail.com
+;;;; 2016/11/09
+;;;;
+;;;; Example of plotting data using GNU plot.
+
+(load "plot")
+
+(defparameter *dataframe* 
+  '((1  1)
+    (1  3)
+    (10 1)
+    (1  4)
+    (11 -4)
+    (1  34)))
+
+(defparameter *fig* (make-instance 'figure))
+(scatter *fig* *dataframe*)
+(arrow *fig* 0 0 10 10)
+(show *fig*)
