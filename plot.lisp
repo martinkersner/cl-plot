300fa7ff9e6b19a80e5f744ee8533ec6b0615062
Martin Keršner <m.kersner@gmail.com>
2016-11-09 23:46:48 +0900

    remove all temporary files after plotting is finished

--- plot.lisp
+++ plot.lisp
@@ -7,6 +7,7 @@
 ;;; 2D scatterplot
 ;;; plot line
 ;;; 3D scatterplot
+;;; create temporary files /tmp directory
 
 (defparameter *output-file* "out") 
 (defparameter *shell* "bash")
@@ -34,7 +35,9 @@
    (circles :accessor set-circles)
    (points :accessor set-points)
    (commands :accessor get-commands
-             :initform '())))
+             :initform nil)
+   (temporary-files :accessor get-temporary-files
+                    :initform nil)))
 
 (defgeneric show (fig)
   (:documentation ""))
@@ -61,7 +64,10 @@
     ;; plot graph
     (ext::shell (concatenate-strings (list *shell* *output-file*)))
   
-    ;; remove temporary file
+    ;; remove from temporary data files
+    (mapcar #'(lambda (tmp-file) (delete-file tmp-file)) (get-temporary-files fig))
+
+    ;; remove temporary command file
     (delete-file stream)))
 
 ;;; SCATTER PLOT
@@ -80,6 +86,8 @@
     (push 
       (concatenate-strings (list "plot" (concatenate 'string "\"" filename "\"") "using 1:2 with points pt 7 ps 2"))
       (get-commands fig))
+
+    (push filename (get-temporary-files fig))
 
   (close stream)))
 
