daaeab57794810dd2c376aefd9e5cc678cc197a6
Martin Keršner <m.kersner@gmail.com>
2016-11-09 23:26:03 +0900

    added simple arrow command and scatter plot

--- plot.lisp
+++ plot.lisp
@@ -2,6 +2,11 @@
 ;;;; 2016/11/09
 ;;;;
 ;;;; Plot data using GNU plot.
+;;;
+;;; TODO
+;;; 2D scatterplot
+;;; plot line
+;;; 3D scatterplot
 
 (defparameter *output-file* "out") 
 (defparameter *shell* "bash")
@@ -13,7 +18,7 @@
          (item (if (stringp item-tmp)
                   item-tmp
                   (write-to-string item-tmp))))
-                                                                                                                                                                         
+
   (if str-lst
     (concatenate-strings
       (cdr str-lst)
@@ -21,25 +26,66 @@
     
     complete)))
 
-;;; main plotting module
-(let ((stream (open *output-file* 
-                    :direction :output 
-                    :if-exists :overwrite
-                    :if-does-not-exist :create)))
+;;; class for plotting
+(defclass figure ()
+  ((pt    :accessor set-pt)
+   (ps    :accessor set-ps)
+   (nokey :accessor set-nokey)
+   (circles :accessor set-circles)
+   (points :accessor set-points)
+   (commands :accessor get-commands
+             :initform '())))
 
-  ;; create temporary file with GNU plot commands
-  ;; demo
-  (write-line "#!/usr/bin/env bash" stream)
-  (write-line "cat << EOF | gnuplot -p" stream)
-  (write-line "splot sin(x*y/20)" stream)
-  (write-line "EOF" stream)
+(defgeneric show (fig)
+  (:documentation ""))
 
-  ;; close file
-  (close stream)
+(defmethod show ((fig figure))
+  (let ((stream (open *output-file* 
+                      :direction :output 
+                      :if-exists :overwrite
+                      :if-does-not-exist :create)))
+  
+    ;; create temporary file with GNU plot commands
+    ;; demo
+    (write-line "#!/usr/bin/env bash" stream)
+    (write-line "cat << EOF | gnuplot -p" stream)
+    ;(write-line "splot sin(x*y/20)" stream)
+    (write-line (concatenate-strings (get-commands fig)) stream)
+    (write-line "EOF" stream)
+  
+    ;; close file
+    (close stream)
+  
+    ;; plot graph
+    (ext::shell (concatenate-strings (list *shell* *output-file*)))
+  
+    ;; remove temporary file
+    (delete-file stream)))
 
-  ;; plot graph
-  (ext::shell (concatenate-strings (list *shell* *output-file*)))
+;;; SCATTER PLOT
+(defgeneric scatter (fig df)
+  (:documentation ""))
 
-  ;; remove temoporary file
-  (delete-file stream))
- 
+(defmethod scatter ((fig figure) df)
+  (let* ((filename "scatter-data.csv")
+         (stream (open filename
+                      :direction :output 
+                      :if-exists :overwrite
+                      :if-does-not-exist :create)))
+
+    (mapcar #'(lambda (row) (write-line (concatenate-strings row) stream)) df)
+
+    (push 
+      (concatenate-strings (list "plot" (concatenate 'string "\"" filename "\"") "using 1:2 with points pt 7 ps 2"))
+      (get-commands fig))
+
+  (close stream)))
+
+;;; ARROW
+(defgeneric arrow (fig X-start Y-start X-end Y-end)
+  (:documentation ""))
+
+(defmethod arrow ((fig figure) X-start Y-start X-end Y-end)
+  (push 
+    (concatenate-strings (list "set arrow from" X-start "," Y-start "to" X-end "," Y-end "nohead"))
+    (get-commands fig)))
